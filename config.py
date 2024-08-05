eee8ccca8c2be8784af6df9e81d07267f2b9455a
Prabhat10122002 <136425797+Prabhat10122002@users.noreply.github.com>
2024-08-05 21:33:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", "29815900"))
-API_HASH = getenv("API_HASH", "5cc2ba67d33f00b22d697d2e04e29975")
-BOT_TOKEN = getenv("BOT_TOKEN", "7403251612:AAE-ksjoOeopUWbN93MgTl7bgrJDZC8VyRI")
-OWNER_ID = int(getenv("OWNER_ID", "1198391218"))
-MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://prabhatsingh10122002:dK4gc4vljnnloIX3@cluster0.0rysthe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-LOG_GROUP = int(getenv("LOG_GROUP", "-1002163104938"))
-FORCESUB = getenv("FORCESUB", "https://t.me/pb_preperation")
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+OWNER_ID = int(getenv("OWNER_ID", ""))
+MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "")
+LOG_GROUP = int(getenv("LOG_GROUP", ""))
+FORCESUB = getenv("FORCESUB", "")
